cdf6ab2afb79dee5c40f09d5d37093fe584e9831
EmeitterHibrido <81694446+EmeitterHibrido@users.noreply.github.com>
2021-10-28 20:18:46 +0100

    Tranbalhando no slider by code EmeitterHibirdo

---
+++ js/codejs.js
@@ -0,0 +1,17 @@
+function menuIcon(){
+    const btn = document.querySelector('#nav-toggler');
+    const collapse = document.querySelector('.nav-collapse');
+
+    collapse.classList.toggle('show-nav')
+}
+
+/* :::: animation && transition while window resizing :::: */
+let resizeTimer;
+window.addEventListener('resize', () => {
+    document.body.classList.add('resize-animation-stopper');
+    clearTimeout(resizeTimer);
+
+    resizeTimer = setTimeout(() => {
+        document.body.classList.remove('resize-animation-stopper');
+    }, 400)
+})
